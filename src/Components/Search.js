b0753deb8183cc42b9bd4f0afcba8806e734e504
Zeljka Raic <zeljka_raic@yahoo.com>
2020-07-13 19:48:09 +0200

    Fetch

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import {apiURL} from "../Common";
 
 export default class Search extends React.Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            query: ""
+        }
     }
+    handleSearch = (e) => {
+        e.preventDefault();
+
+        if(!this.state.query || this.state.query.length === 0){
+            return;
+        }
+    
+        Promise.all([
+            fetch(apiURL + this.state.query)
+            .then((response) => response.json()),
+            fetch(apiURL + this.state.query + "/repos")
+            .then((response) =>
+              response.json()
+            ),
+          ]).then(([data, repo]) => {
+            this.props.onResult(data, repo);
+            this.setState({ query: "" });
+          });
+   
+}
 
     render(){
         return(
             <form>
-                
+
             </form>
 
 
