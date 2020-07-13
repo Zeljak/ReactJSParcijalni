9b457b64bf7bdeee8ad790d607371e905b62ac5b
Zeljka Raic <zeljka_raic@yahoo.com>
2020-07-13 20:05:32 +0200

    list-data

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-export default function List (){
+export default function List ({ data, repo}){
+    if (!data || data.length === 0){
+        return "Sorry, but there were no results";
+    }
 
 
 
 return(
-    <ul></ul>
+    <ul>
+        <ul>
+ <img src={data["avatar_url"]} alt={data.name}/>
+ <li>NAME: {data.name}</li>
+ <li>LOCATION: {data.location}</li>   
+ <li>BIO: {data.bio}</li>  
+ </ul>
+     <ul>
+       {repo.map((user)=> {
+         return <li key={user.id + " " + user.node_id}>REPOSITORY: {user.name}</li>  
+       })}
+     </ul>
+    </ul>
 );
 
 }
